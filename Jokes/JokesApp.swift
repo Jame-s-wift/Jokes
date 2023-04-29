1e46d92602a2e92c3b68bbf20abf4aa3b6fbae2e
James <jtyflol@gmail.com>
2023-04-29 11:58:00 +0800

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by James Toh on 29/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
